e1307173c8b75cc6ae119d062abcd86a62652da2
cexes <mottacesar001@gmail.com>
2021-10-02 02:11:39 -0300

    sub_soma

---
+++ cod.js
@@ -0,0 +1,14 @@
+function soma(number,number2){
+    number = parseInt(number)
+    number2 = parseInt(number2)
+    return result= number +number2
+}
+ function sub(number,number2){
+     number = parseInt(number)
+     number2 = parseInt(number2)
+    return result =  number  - number2}
+  if(soma_sub =="soma"){
+      document.write("O resultado da soma:" + soma(number,number2))
+  } if(soma_sub =="subtracao"){
+      document.write("O resultado da sub: " + sub(number,number2))
+  }
